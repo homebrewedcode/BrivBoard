96b0e372dadc01530c86a4dcc8db9e03c1cd7456
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-07 18:22:44 -0700

    validate firstname and lastname, style messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
