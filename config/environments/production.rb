c0ae5b53cdad77cb17a5d923db1047af389f54a6
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-08 18:24:48 -0700

    config production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  #setup mail on heroku
+  config.action_mailer.default_url_options = { :host => 'brivboard-homebrewedcode.herokuapp.com' }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
